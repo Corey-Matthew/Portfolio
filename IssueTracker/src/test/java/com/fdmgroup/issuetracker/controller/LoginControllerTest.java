1db13b83ff831c6b5c1e8a963ffa16c949f0b3c1
corey.davis <corey.davis@TRNNYC30216.fdmgroup.local>
2018-01-03 17:26:11 -0500

    adding controller test

--- IssueTracker/src/test/java/com/fdmgroup/issuetracker/controller/LoginControllerTest.java
+++ IssueTracker/src/test/java/com/fdmgroup/issuetracker/controller/LoginControllerTest.java
@@ -1,15 +1,39 @@
-package com.fdmgroup.issuetracker.controller;
+package com.fdmgroup.issueTracker.controller;
 
 import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.context.WebApplicationContext;
 
+import com.fdmgroup.issuetracker.model.impl.User;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("context.xml")
+@WebAppConfiguration
 public class LoginControllerTest {
-
+	@Autowired
+    private WebApplicationContext wac;
+	
+	private MockMvc mockMvc;
+	private User user;
 	@Before
 	public void setUp() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+		
 	}
 
 	@After
@@ -17,8 +41,13 @@ public class LoginControllerTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIfDisplayLoginPageReturnsLoginJsp() throws Exception {
+	mockMvc.perform(get("/login")).andExpect(status().isOk()).andExpect(model().attribute("user", User.class)).andExpect(content().string("login"));
+	}
+	
+	@Test
+	public void testLoginServelt() throws Exception {
+	mockMvc.perform(get("/loginServlet")).andExpect(status().isOk()).andExpect(model().attribute("user", User.class)).andExpect(content().string("login"));
 	}
 
 }
