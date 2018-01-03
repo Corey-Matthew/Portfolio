3f2469d8657f21bf6ab7841653c4b38a7ba15b52
Kyle Shermer <kyle.shermer@fdmgroup.com>
2018-01-03 10:27:11 -0500

    update

--- IssueTracker/src/main/java/com/fdmgroup/issuetracker/model/impl/UserDAO.java
+++ IssueTracker/src/main/java/com/fdmgroup/issuetracker/model/impl/UserDAO.java
@@ -12,46 +12,28 @@ import javax.persistence.TypedQuery;
 //import org.apache.log4j.Logger;
 //import org.apache.log4j.PropertyConfigurator;
 
+import com.fdmgroup.issuetracker.model.IUserDAO;
+
 /**
  * Implements UserDAO interfaces to manipulate User table in the database
  *
  */
-public class UserDAO {
+public class UserDAO  {
 
 	private static final String PERSISTENCE_UNIT_NAME = "IssueTracker";
-	// static Logger myLogger = Logger.getLogger("myLogger");
-
+//	static Logger myLogger = Logger.getLogger("myLogger");
+	
 	private static EntityManagerFactory factory;
 
-	public UserDAO() {
-		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+	public UserDAO(){		
+		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);	
 	}
 
 	public EntityManager getEntityManager() {
 		return factory.createEntityManager();
 	}
 
-	/**
-	 * Insert a row with the user information to the User table
-	 */
-	public void addUser(User user) {
 
-		int userId = user.getUserId();
-		Department department_id = user.getDepartment();
-		String username = user.getUsername();
-		String password = user.getPassword();
-		String email = user.getEmail();
-
-		EntityManager em = factory.createEntityManager();
-
-		try {
-			em.getTransaction().begin();
-			em.persist(user);
-			em.getTransaction().commit();
-		} finally {
-			em.close();
-		}
-	}
 	
 	/**
 	 * Retrieves a user with the passed in parameter of username
@@ -67,20 +49,25 @@ public class UserDAO {
 		}
 	}
 
+
 	/**
 	 * Removes a user with the passed in parameter of username
 	 */
-	public boolean removeUser(String username) {
 
-		EntityManager em = factory.createEntityManager();
-		User user = em.find(User.class, username);
-
+	/**
+	 * Update a user if the username exist in the database
+	 */
+	public boolean updateUser(User user) {
+		EntityManager em = getEntityManager();
+		EntityTransaction et = em.getTransaction();
+		User foundUser = getUser(user.getUsername());
 		try {
-			if (user != null) {
-
-				em.getTransaction().begin();
-				em.remove(user);
-				em.getTransaction().commit();
+			if (foundUser != null) {
+				et.begin();
+				User modifyUser = em.find(User.class, foundUser.getUserId());
+				modifyUser.setPassword(user.getPassword());
+				modifyUser.setEmail(user.getEmail());
+				et.commit();
 				return true;
 			}
 		} finally {
@@ -90,11 +77,14 @@ public class UserDAO {
 	}
 
 	/**
-	 * Returns a list of users. SQL query is found in the User class in
-	 * NamedQueries
+	 * Returns a list of users.
+	 * SQL query is found in the User class in NamedQueries
 	 */
 	public List<User> listUsers() {
 		TypedQuery<User> query = getEntityManager().createNamedQuery("User.findAll", User.class);
 		return query.getResultList();
 	}
+
+
+
 }
