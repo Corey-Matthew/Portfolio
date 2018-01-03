697e09105ff382133559f7d180211e54ee2c8cbd
Kyle Shermer <kyle.shermer@fdmgroup.com>
2018-01-03 09:03:25 -0500

    Adding userDAO

--- IssueTracker/src/main/java/com/fdmgroup/issuetracker/model/impl/UserDAO.java
+++ IssueTracker/src/main/java/com/fdmgroup/issuetracker/model/impl/UserDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
@@ -22,15 +23,37 @@ public class UserDAO{
 	public User getUser(String username){
 		User use;
 		TypedQuery<User> type = getEntityManager().createNamedQuery("User.findByName", User.class);
-		use = type.setParameter("username",username).getSingleResult();
-				
-		return use;
+		try{
+			use = type.setParameter("username",username).getSingleResult();
+			return use;
+		}catch(NoResultException e){
+			return null;
+		}
+		
 	}
 	public List<User> listUsers(){
 		TypedQuery<User> type = getEntityManager().createNamedQuery("User.findAll", User.class);
 		return type.getResultList();
 	}
-	public void updateUser(){
+	public boolean updateUser(User use){
+		EntityManager em = getEntityManager();
+		EntityTransaction et = em.getTransaction();
+		User foundUser = getUser(use.getUsername());
+		
+		try{
+			
+			if(foundUser != null){
+				User newUser = em.find(User.class, use.getUserId());
+				et.begin();
+				newUser.setPassword(use.getPassword());
+				newUser.setEmail(use.getEmail());
+				et.commit();
+				return true;
+			}
+		}finally{
+			em.clear();
+		}
+		return false;
 		
 	}
 }
