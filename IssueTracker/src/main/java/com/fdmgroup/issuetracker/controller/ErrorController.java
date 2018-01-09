c5de7489fbb422f3caf27e2472f67bfc74381e11
Jeremy.Castro <Jeremy.Castro@fdmgroup.com>
2018-01-09 10:36:57 -0500

    error page controller added

---
+++ IssueTracker/src/main/java/com/fdmgroup/issuetracker/controller/ErrorController.java
@@ -0,0 +1,30 @@
+package com.fdmgroup.issuetracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Controller class for errors and exceptions
+ * @author Knight
+ *
+ */
+@Controller
+public class ErrorController {
+	
+	/**
+	 * Method that directs to the error page 
+	 * @return "error" (error.jsp)
+	 */
+	@RequestMapping(value="/error")
+	public String goToError(){
+		return "error";
+	}	
+	/**
+	 * Method that directs to exception page
+	 * @return "exception" (exception.jsp)
+	 */
+	@RequestMapping(value="/exception")
+	public String goToException(){
+		return "exception";
+	}	
+}
